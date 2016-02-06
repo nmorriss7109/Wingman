9c9bfb471f6b708524d4fc2c2379355c8fb95342
Nathan <cybermancan@gmail.com>
2016-02-05 19:34:06 -0800

    Added get_face

--- wingman.py
+++ wingman.py
@@ -1,11 +1,18 @@
+import urllib
 import pynder
+import os.path
 
-FBTOKEN = 'CAAGm0PX4ZCpsBAB55ZAL2tuyB9vuWjPKIdEjw7xGLGZAE1M6IgFD1ZB7ZBcgZB0pxnXnTEEWnTndkNVW7uWremhUlneSa3elJ9GrsOejYFA7yR0OfPNiMb3QiIOcpo4WTBGex27Q0KJoXTYZBlrLNzRuhqqGaDA7R6FQ6v4AKe0bHpK7Vq8Na8zaiq5QpOQ4VIZCwM8HhWifbwZDZD'
+
+FBTOKEN = 'CAAGm0PX4ZCpsBAC1wdZASOlfEBKgKmkPHQk4ydsyc2rC9t70vQJ3Ia7rzS6Tkl60vNjt3cwj6NxBYXxXAMTF2ZAj9xEtFuJb1n9QDqMLSx4FTdXbrZAPni8Kl9gyOLwupwAWkZCF4LfWyfKvDHT5eJMy5iAYMXk8fNrwH04CXRYd6znDn4LMcTnqokzu3mEG6eM6ZBFAQMeQZDZD'
 FBID = '464891386855067'
 
 session = pynder.Session(FBID, FBTOKEN)
-users = session.nearby_users()
 
-for user in users[:1]:
-	print user.name
-	print user.get_photos(width='640')
+def get_photos(users):
+	for user in users:
+		print user.name
+		img = user.get_photos(width='640')
+		for i in range(0, len(img)):
+			urllib.urlretrieve(img[i], os.path.join('faces', user.name + str(i) + '.jpg'))
+
+get_photos(session.nearby_users())
